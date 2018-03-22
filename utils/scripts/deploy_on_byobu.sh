8644047017f26d0f37adda84b1ed0b7eef023bb0
Adrián Roselló <adrian.rosello@i2cat.net>
2018-03-22 10:07:43 +0100

    Add ansible playbook for the GUI

--- utils/scripts/deploy_on_byobu.sh
+++ utils/scripts/deploy_on_byobu.sh
@@ -5,3 +5,5 @@ byobu new-session -d -s $USER
 byobu rename-window -t $USER:0 'BACKEND'
 byobu send-keys "cd ~/code/rumba/backend && source venv/bin/activate && python api/api_controller.py" C-m
 
+byobu new-window -t $USER:1 -n 'FRONTEND'
+byobu send-keys "cd ~/code/rumba/rumba-front && source ~/.nvm/nvm.sh && nvm use v7.8.0 && ng serve" C-m
