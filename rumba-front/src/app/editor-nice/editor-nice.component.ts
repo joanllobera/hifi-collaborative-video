b94618bb8eac6b37cf0848538d610a940cbb1323
enric-alminyana <enric.alminyana@i2cat.net>
2018-04-18 14:40:55 +0200

    get thumbnails for one video dinamically

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import * as JSZip from 'jszip';
+import { VideosServiceService } from '../videos-service.service';
+
+import * as FileSaver from 'file-saver';
+
 @Component({
   selector: 'app-editor-nice',
   templateUrl: './editor-nice.component.html',
@@ -7,15 +12,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditorNiceComponent implements OnInit {
 
-  constructor() { }
+  urls: any = undefined;
+  list: any[] = [];
+  check: any = undefined;
+
+
+
+  constructor(private videoService: VideosServiceService) { }
 
   ngOnInit() {
+    //this.onGetThumbnails();
   }
+
 
   onSelectFrame(event) {
     event.target.classList.toggle('selectedImg');
   }
 
+  onGetThumbnails() {
+    this.videoService.getThunmbnailsFromVideo()
+      .subscribe(
+        (response) => {
+          var new_zip = new JSZip();
+          new_zip.loadAsync(response)
+          .then(
+            (zip) => {
+
+              var sortedObject = [];
+              let zipFiles = zip.files;
+              const ordered = {};
+              Object.keys(zipFiles).sort().forEach(function(key) {
+                ordered[key] = zipFiles[key];
+              });
+
+              for (var prop in ordered) {
+                let blob = new Blob( [ ordered[prop]._data.compressedContent ], { type: "image/jpeg" } );
+                let reader = new FileReader();
+                reader.addEventListener("load", () => {
+                  if (reader.result != "") {
+                    this.list.push(reader.result);
+                  }
+                }, false);
+                if (blob) {
+                   reader.readAsDataURL(blob);
+                }
+              }
+          });
+
+          // download zip file
+          let fileName = "QCPReport.zip";
+          FileSaver.saveAs(response, fileName);
+
+        },
+        (error) => {
+          console.log('error::::', error);
+        }
+      );
+
+  }
 
 
 }
--- rumba-front/src/app/videos-service.service.ts
+++ rumba-front/src/app/videos-service.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 import { Video } from './model/video.model';
 import { Vimeo } from './model/vimeo.model';
 
+import * as JSZip from 'jszip';
+import { HttpClient } from '@angular/common/http';
+
+import 'rxjs/Rx';  //needed for .map()
+
+import { AppConfig } from './app-config';
+import { Http, ResponseContentType } from '@angular/http';
+
 @Injectable()
 export class VideosServiceService {
 
@@ -12,10 +20,21 @@ export class VideosServiceService {
     new Video(3, 'Primavera Club', 'The Punsetes', 'Apolo', new Vimeo('aaa', 'bbb'))
   ];
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private http: Http) { }
 
   getVideos() {
     return this.listOfVideos.slice();
   }
+
+  getThunmbnailsFromVideo() {
+    return this.httpClient.get(AppConfig.API_ENDPOINT + AppConfig.API_VERSION + '/video/' + '5ad4b5fdc94b4c6bc260dd3c' + '/thumbs', {observe: 'body', responseType: 'blob'}).map(
+        (response) => {
+          console.log(response);
+          return response;
+        }
+      );
+  }
+
+
 
 }
