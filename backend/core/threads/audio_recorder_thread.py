e612c34a247eeb407dd2f0d1b6d4243e796199f1
Adrián Roselló <adrian.rosello@i2cat.net>
2018-04-23 16:56:12 +0200

    Directly write the audio to a file.

--- backend/core/threads/audio_recorder_thread.py
+++ backend/core/threads/audio_recorder_thread.py
@@ -15,7 +15,6 @@ class AudioRecorderThread(Thread):
     audio = None
     stream = None
     recording = False
-    frames = []
 
     def __init__(self, session_path):
         super(AudioRecorderThread, self).__init__()
@@ -25,10 +24,16 @@ class AudioRecorderThread(Thread):
     def run(self):
         self.stream = self.audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,
                                  frames_per_buffer=CHUNK)
+        waveFile = wave.open(self.session_path + WAVE_OUTPUT_FILENAME, "wb")
+        print("Recording in file: {}".format(self.session_path + WAVE_OUTPUT_FILENAME))
+        waveFile.setnchannels(CHANNELS)
+        waveFile.setsampwidth(self.audio.get_sample_size(FORMAT))
+        waveFile.setframerate(RATE)
         self.recording=True
         while self.recording:
             data = self.stream.read(CHUNK)
-            self.frames.append(data)
+            waveFile.writeframes(data)
+        waveFile.close()
         self.stop()
         return
 
@@ -38,12 +43,4 @@ class AudioRecorderThread(Thread):
         self.stream.stop_stream()
         self.stream.close()
         self.audio.terminate()
-        waveFile = wave.open(self.session_path + WAVE_OUTPUT_FILENAME, "wb")
-        waveFile.setnchannels(CHANNELS)
-        waveFile.setsampwidth(self.audio.get_sample_size(FORMAT))
-        waveFile.setframerate(RATE)
-        waveFile.writeframes(b''.join(self.frames))
-        waveFile.close()
-        self.audio.terminate()
-        self.frames = []
         self.stream = None
