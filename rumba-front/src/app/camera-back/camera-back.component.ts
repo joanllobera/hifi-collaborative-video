58a7fda4ca53717f33aa58c07d8cd0cdb9f966bb
enric-alminyana <enric.alminyana@i2cat.net>
2018-05-03 10:43:24 +0200

    configure janus on click button

--- rumba-front/src/app/camera-back/camera-back.component.ts
+++ rumba-front/src/app/camera-back/camera-back.component.ts
@@ -29,7 +29,7 @@ export class CameraBackComponent implements OnInit {
   }
 
   startRecording() {
-    //this.configureJanus();
+    this.configureJanus();
 
     // this.record.startRecordingVideo()
     //   .subscribe(
@@ -253,7 +253,7 @@ export class CameraBackComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.configureJanus();
+    //this.configureJanus();
   }
 
 
