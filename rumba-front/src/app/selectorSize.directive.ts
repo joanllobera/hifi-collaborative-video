9f9eed3f70b3b5237b9243f7a4481080079d0f8e
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-26 13:23:46 +0200

    new directive

---
+++ rumba-front/src/app/selectorSize.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, Input, ElementRef, OnInit } from '@angular/core';
+
+@Directive({
+	selector: '[appSelectorSize]'
+})
+export class SelectorSizeDirective implements OnInit {
+
+  constructor (private elementRef: ElementRef) {}
+
+	ngOnInit(): void {
+    this.elementRef.nativeElement.style.width;
+	}
+
+  
+
+}
