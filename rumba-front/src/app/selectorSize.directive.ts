685f73f4f0c588a57c318ee1f570fbf6f4f66c4b
enric-alminyana <enric.alminyana@i2cat.net>
2018-07-30 15:33:19 +0200

    added touch events plus mouse events

--- rumba-front/src/app/selectorSize.directive.ts
+++ rumba-front/src/app/selectorSize.directive.ts
@@ -58,6 +58,8 @@ export class SelectorSizeDirective implements OnInit {
 		function dragElement(elmnt) {
 		  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
 		  elmnt.onmousedown = dragMouseDown;
+			elmnt.ontouchstart = dragMouseDown;
+
 
 		  function dragMouseDown(e) {
 		    e = e || window.event;
@@ -66,9 +68,12 @@ export class SelectorSizeDirective implements OnInit {
 		    pos3 = e.clientX;
 		    pos4 = e.clientY;
 		    document.onmouseup = closeDragElement;
-		    // call a function whenever the cursor moves:
+				document.ontouchend = closeDragElement;
+
+				// call a function whenever the cursor moves:
 		    document.onmousemove = elementDrag;
-		  }
+				document.ontouchmove = elementDrag;
+			}
 
 		  function elementDrag(e) {
 		    e = e || window.event;
