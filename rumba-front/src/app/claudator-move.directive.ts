d4c0f2f1da1ba2f1f76513f61d38554da6c5671d
enric-alminyana <enric.alminyana@i2cat.net>
2018-09-05 13:51:03 +0200

    move claudator

--- rumba-front/src/app/claudator-move.directive.ts
+++ rumba-front/src/app/claudator-move.directive.ts
@@ -10,6 +10,7 @@ export class ClaudatorMoveDirective implements OnInit {
   constructor(private elementRef: ElementRef) {}
 
   ngOnInit() {
+    this.moveSelector();
   }
 
   @HostListener('click', ['$event'])
@@ -19,39 +20,83 @@ export class ClaudatorMoveDirective implements OnInit {
 
   @HostListener('mousemove', ['$event'])
 	onMouseMove(event: MouseEvent) {
-		//console.log('moving', event);
 
 	}
 
 	@HostListener('touchstart', ['$event'])
 	onTouchstart(event: MouseEvent) {
-		//console.log('moving', event);
-		this.elementRef.nativeElement.style.left = event.pageX + 'px';
 
 	}
 
 	@HostListener('touchend', ['$event'])
 	onTouchend(event: MouseEvent) {
-		//console.log('moving', event);
 
 	}
 
 	@HostListener('dblclick', ['$event'])
 	onMouseDoubleClick(event: MouseEvent) {
-		console.log('double click', event);
 
 	}
 
 	@HostListener('mousedown', ['$event'])
 	onMouseDown(event: MouseEvent) {
-		console.log('mousedown', event);
 
 	}
 
 	@HostListener('mouseup', ['$event'])
 	onMouseUp(event: MouseEvent) {
-		console.log('mouseup', event);
 
+	}
+
+
+
+  moveSelector() {
+		//Make the DIV element draggagle:
+		//dragElement(document.getElementById("mySelector"));
+		dragElement(this.elementRef.nativeElement)
+
+		function dragElement(elmnt) {
+		  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+		  elmnt.onmousedown = dragMouseDown;
+			elmnt.ontouchstart = dragMouseDown;
+
+		  function dragMouseDown(e) {
+		    e = e || window.event;
+		    e.preventDefault();
+		    // get the mouse cursor position at startup:
+		    pos3 = e.clientX;
+		    pos4 = e.clientY;
+		    document.onmouseup = closeDragElement;
+				document.ontouchend = closeDragElement;
+
+				// call a function whenever the cursor moves:
+		    document.onmousemove = elementDrag;
+				document.ontouchmove = elementDrag;
+			}
+
+		  function elementDrag(e) {
+		    e = e || window.event;
+		    e.preventDefault();
+		    // calculate the new cursor position:
+		    pos1 = pos3 - e.clientX;
+		    pos2 = pos4 - e.clientY;
+		    pos3 = e.clientX;
+		    pos4 = e.clientY;
+		    // set the element's new position:
+		    //elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		  }
+
+		  function closeDragElement() {
+		    /* stop moving when mouse button is released:*/
+		    document.onmouseup = null;
+		    document.onmousemove = null;
+
+				//touch events
+				document.ontouchend = null;
+				document.ontouchmove = null;
+		  }
+		}
 	}
 
 
