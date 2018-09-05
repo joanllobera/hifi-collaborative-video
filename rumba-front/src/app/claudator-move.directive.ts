c622c7ae315e65948ce721c0060fbf5da97fb01c
enric-alminyana <enric.alminyana@i2cat.net>
2018-09-05 12:07:38 +0200

    try mousedown

--- rumba-front/src/app/claudator-move.directive.ts
+++ rumba-front/src/app/claudator-move.directive.ts
@@ -11,21 +11,48 @@ export class ClaudatorMoveDirective implements OnInit {
 
   }
 
-  @HostListener('touchstart', ['$event'])
-  onTouchstart (event: MouseEvent) {
-
-  }
-
-  @HostListener('mousedown', ['$event'])
-  onMouseDown(event: MouseEvent) {
-
-  }
-
   @HostListener('click', ['$event'])
   onClick(event: MouseEvent) {
-    this.elementRef.nativeElement.style.left = event.pageX + 'px';
+
   }
 
+  @HostListener('mousemove', ['$event'])
+	onMouseMove(event: MouseEvent) {
+		//console.log('moving', event);
+
+	}
+
+	@HostListener('touchstart', ['$event'])
+	onTouchstart(event: MouseEvent) {
+		//console.log('moving', event);
+		alert('touchstart');
+
+	}
+
+	@HostListener('touchend', ['$event'])
+	onTouchend(event: MouseEvent) {
+		//console.log('moving', event);
+		alert('touchend');
+
+	}
+
+	@HostListener('dblclick', ['$event'])
+	onMouseDoubleClick(event: MouseEvent) {
+		console.log('double click', event);
+
+	}
+
+	@HostListener('mousedown', ['$event'])
+	onMouseDown(event: MouseEvent) {
+		console.log('mousedown', event);
+    this.elementRef.nativeElement.style.left = event.pageX + 'px';
+	}
+
+	@HostListener('mouseup', ['$event'])
+	onMouseUp(event: MouseEvent) {
+		console.log('mouseup', event);
+
+	}
 
 
 }
