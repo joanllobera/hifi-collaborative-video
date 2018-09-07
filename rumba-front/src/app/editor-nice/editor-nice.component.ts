08ef49e7998dff95fc9cdb11ddafda6de2fa34d2
enric-alminyana <enric.alminyana@i2cat.net>
2018-09-07 11:21:57 +0200

    check if array exists and has length grather than 0

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -200,7 +200,7 @@ export class EditorNiceComponent implements OnInit {
     }
 
     let imgLast = video.querySelectorAll('img.selectedImg');
-    if (imgLast) imgLast[imgLast.length-1].classList.add('last');
+    if (imgLast.length > 0) imgLast[imgLast.length-1].classList.add('last');
 
   }
 
