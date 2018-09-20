e732e9ce4a33337ee8ba7b1fff890d73146506c0
enric-alminyana <enric.alminyana@i2cat.net>
2018-09-20 12:04:44 +0200

    fix for

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -78,7 +78,7 @@ export class EditorNiceComponent implements OnInit {
   }
 
   getAllAreSame(singleArray, zoom: number) {
-    for (let i = 0; i < singleArray.length; i = i + zoom - 1) {
+    for (let i = 0; i < singleArray.length; i = i + zoom) {
       console.log('currentI', i);
       for (let j = 0; j < zoom - 1; j++) {
         // console.log(singleArray[i + j]);
