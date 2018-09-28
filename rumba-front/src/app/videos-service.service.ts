0dbe5884ac3f14ae3bcdd373b7deda60f5abc5b9
enric-alminyana <enric.alminyana@i2cat.net>
2018-09-28 16:11:12 +0200

    url

--- rumba-front/src/app/videos-service.service.ts
+++ rumba-front/src/app/videos-service.service.ts
@@ -55,12 +55,8 @@ export class VideosServiceService {
   }
 
   sendVideoToBuild(json, session_id) {
-    // return this.httpClient.post(
-    //   AppConfig.API_ENDPOINT + AppConfig.API_VERSION + '/edition/' + session_id + '/build', json, {observe: 'body', responseType: 'json'}
-    //   );
-
     return this.httpClient.post(
-      AppConfig.API_ENDPOINT + AppConfig.API_VERSION + '/' + session_id + '/build', json, {observe: 'body', responseType: 'json'}
+      AppConfig.API_ENDPOINT + AppConfig.API_VERSION + '/edition/' + session_id + '/build', json, {observe: 'body', responseType: 'json'}
       );
   }
 
