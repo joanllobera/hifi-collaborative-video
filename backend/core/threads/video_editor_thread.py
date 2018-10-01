fbbc6fd979307f987259f9d37e8decaad4a89ba8
Daniel Albarral Nunez <daniel.albarral@i2cat.net>
2018-10-01 12:00:02 +0200

    fix retrieve video mixed

--- backend/core/threads/video_editor_thread.py
+++ backend/core/threads/video_editor_thread.py
@@ -1,10 +1,15 @@
 import subprocess
+import uuid
 from threading import Thread
 from time import sleep
 
 from core.exceptions.generic_exceptions import NotExistingResource
+from core.helpers.data_transformer import DataTransformer
 from core.helpers.loggers import LoggerHelper
+from core.helpers.video_helper import VideoEditorHelper
 from core.model.rumba_session import RumbaSession
+from core.services.audio_manager import AudioManager
+from core.threads.audio_splitter_thread import AudioSplitterThread
 from core.threads.audio_video_mixer_thread import AudioVideoMixerThread
 
 LOGGER = LoggerHelper.get_logger("video_editor", "video_editor.log")
@@ -25,6 +30,41 @@ class VideoEditorThread(Thread):
         self.output_file = output_file
         self.command = "ffmpeg -f concat -safe 0 -i {} {}".format(self.edition_info_filename,
                                                           self.output_file)
+    @staticmethod
+    def __cut_audio__(session_id, edit_info):
+        """
+        Given a rumba session and the information to mount a video, it cuts the session audio to
+        fit into the mounted video. For this, it takes into account the timestamp of the
+        first frame of the video to mount and the timestamp of the last frame of this new video.
+
+        VERY IMPORTANT: This method assumes that the timestamps of video and audio are consistent
+        in terms of synchronization.
+
+        :param session_id: Id of the rumba session.
+        :param edit_info: The informtion provided by the editor containing the list of video slices.
+        :return: Absolute path where the audio cut by this method is located.
+        """
+        session = RumbaSession.objects(id=session_id).first()
+        if session is None:
+            raise NotExistingResource("There's no session with such id.")
+        audio_path = "{}/audio.wav".format(session['folder_url'])
+        video_init_ts = VideoEditorHelper.get_first_video_ts(edit_info=edit_info)
+        audio_init_ts = AudioManager.get_instance().get_audio_init_ts(session_id=session_id)
+        audio_init_offset = VideoEditorHelper.calculate_audio_init_offset(audio_init_ts=audio_init_ts,
+                                                                          video_init_ts=video_init_ts)
+        ffmpeg_audio_init_offset = DataTransformer.transform_seconds_to_ffmpeg_offset(float(audio_init_offset))
+        audio_init_ts = AudioManager.get_instance().get_audio_init_ts(session_id=session_id)
+        audio_end_offset = VideoEditorHelper.calculate_audio_end_offset(audio_init_ts=audio_init_ts,
+                                                                        edit_info=edit_info,
+                                                                        audio_init_offset=audio_init_offset)
+        audio_output = "{}/audio-{}.wav".format(session['folder_url'], uuid.uuid4().hex)
+        audio_thread = AudioSplitterThread(inputFile=audio_path, outputFile=audio_output,
+                                           initial_offset=ffmpeg_audio_init_offset, end_offset=audio_end_offset)
+        audio_thread.start()
+        audio_thread.join()
+        if audio_thread.code != 0:
+            raise Exception("FFMpeg command failed.")
+        return audio_output
 
     def run(self):
         LOGGER.info("VideoEditorThread: Cutting and concataniting video slices.")
