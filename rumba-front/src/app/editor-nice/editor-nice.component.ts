3a060f3cd9dfe22e020c94148aedc963e08e2bd6
enric-alminyana <enric.alminyana@i2cat.net>
2018-11-06 12:25:56 +0100

    try3

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -645,7 +645,7 @@ export class EditorNiceComponent implements OnInit {
             (zip, index) => {
               // return zip.files;
               this.zipList.push(zip.files);
-              this.listOfLists.push(zip.files);
+              // this.listOfLists.push(zip.files);
           });
           // console.log('zip.files::::::::', a);
         },
