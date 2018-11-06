272a23801a1454fa833d6150f5e34cf0101600bd
enric-alminyana <enric.alminyana@i2cat.net>
2018-11-06 12:14:21 +0100

    fix order try1

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -592,6 +592,21 @@ export class EditorNiceComponent implements OnInit {
               console.log('ordered::', ordered);
               console.log('allKeys::', allKeys);
 
+              // for (const prop in ordered) {
+              //   if (ordered.hasOwnProperty(prop)) {
+              //     const blob = new Blob( [ ordered[prop]._data.compressedContent ], { type: 'image/jpeg' } );
+              //     const reader = new FileReader();
+              //     reader.addEventListener('load', () => {
+              //       if (reader.result !== '') {
+              //         temp.push(reader.result);
+              //       }
+              //     }, false);
+              //     if (blob) {
+              //       reader.readAsDataURL(blob);
+              //     }
+              //   }
+              // }
+
               for (const prop in ordered) {
                 if (ordered.hasOwnProperty(prop)) {
                   const blob = new Blob( [ ordered[prop]._data.compressedContent ], { type: 'image/jpeg' } );
@@ -601,11 +616,15 @@ export class EditorNiceComponent implements OnInit {
                       temp.push(reader.result);
                     }
                   }, false);
-                  if (blob) {
-                    reader.readAsDataURL(blob);
-                  }
+                  // if (blob) {
+                  //   reader.readAsDataURL(blob);
+                  // }
                 }
               }
+
+
+
+
               this.listOfLists.push(temp);
           });
         },
