2a316a3e4d7b2dc56e32cd244998b22e5bf74e15
enric-alminyana <enric.alminyana@i2cat.net>
2018-11-07 13:08:51 +0100

    accessing only to first video

--- rumba-front/src/app/editor-nice/editor-nice.component.ts
+++ rumba-front/src/app/editor-nice/editor-nice.component.ts
@@ -217,7 +217,7 @@ export class EditorNiceComponent implements OnInit {
             return each.ts > 0;
           });
 
-          const initialTime: number = this.allVideosOk['ts'].toFixed(1);
+          const initialTime: number = this.allVideosOk[0]['ts'].toFixed(1);
 
           this.allVideosOk.forEach((each, index) => {
             // console.log('eachVideo :::' + index, each);
